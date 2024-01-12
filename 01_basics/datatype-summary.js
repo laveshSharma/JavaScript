2616c286a6bdbc2a35f416f166f57a095a50a5ad
laveshSharma <144525063+laveshSharma@users.noreply.github.com>
2024-01-12 10:31:20 +0000

    summarize dataType

---
+++ 01_basics/datatype-summary.js
@@ -0,0 +1,23 @@
+// javaScript is a dynamically typed language which means we can't define the type of data js automatically detect it.
+
+/*
+dataType summary:
+
+Primitve:- 7 types = number, boolean, string, null, undefined, symbol, bigInt.
+
+Reference(non-primitive):- Array, Function, Objects.
+
+typeof tells the dataType of a Variable.
+
+non-primitive ka data type hamesa object hota hai par function ka object function hota hai.
+
+*/
+const heros =["shkatiman", "IronMan","Captain-America"];
+console.log(typeof heros);
+
+
+
+
+
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
