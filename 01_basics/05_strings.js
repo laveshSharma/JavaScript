a60c6bdfd4bfb69ac402f93c1e34ffa0b70fd5ee
Lavesh Sharma <144525063+laveshSharma@users.noreply.github.com>
2024-01-12 15:52:30 +0000

    string basic discussion

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,16 @@
+const name = "Lavesh"
+const repoCount = 50;
+
+// console.log("Hi my name is" +" " + name + " " + " and my repo count is" + " " + repoCount );
+
+// // using Template literal
+// console.log(`Hi my name is ${name} and my repo count is ${repoCount} `);
+
+const gameName = new String("hiteshhc");
+
+console.log(gameName.length);
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('t'));
+console.log(gameName.toUpperCase());
+
+
