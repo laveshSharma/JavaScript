46a4526e742d9004e3e6ef1cbe09efa851fed557
laveshSharma <144525063+laveshSharma@users.noreply.github.com>
2024-01-12 09:12:09 +0000

    discussion on datatype

---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all js code as a newer version
+
+// alert(3+4) we are using nodejs, not console or browser
+
+// code readability is important
+console.log(3+
+    3); console.log("lavesh");//bad pratice of writing code
+
+/*
+Data type : there are two type of data type in js
+1) object: dicuss in deatil after sometime
+2) primitive: following are the primitive data type
+    a)number => range of number 2^53 (we maximum time use number dataType)
+    b)string
+    c)boolean => true or false
+    d)undefined
+    e)null
+    f)bigInt
+    g)Symbol => unique
+
+typeof operator tells the dataType of the particular value.
+
+*/
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
