5141e8e895d7055c7da0dc91997dd4f22ec295e4
laveshSharma <144525063+laveshSharma@users.noreply.github.com>
2024-01-12 10:12:35 +0000

    comparision discussion

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,17 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 <= 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// make a rule never compare two different dataType like
+console.log("2" > 1);
+
+// equlity operator and comparision operator work differently study in detail later.
+
+/*
+there are two type of equality operator 
+loosely equality ==
+stict equality === (it check strictly compate which means it check the dataType and value also)
+*/
